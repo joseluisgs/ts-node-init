d155bb02b0519f31fb4d949c8f2897a33ae47e61
joseluisgs <joseluisgonsan@gmail.com>
2020-12-28 08:23:47 +0100

    Opciones JS y librerías

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-console.log('hola');
+console.log('Prueba');
--- ts-init.sh
+++ ts-init.sh
@@ -2,5 +2,5 @@
 # Para crear el fichero tsconfig.json
 
 npx tsc --init --rootDir src --outDir build \
---esModuleInterop --resolveJsonModule --lib es6 \
+--esModuleInterop --resolveJsonModule --lib ES6 \
 --module commonjs --allowJs true --noImplicitAny true
